2c77f11a432eec2fc66a37f43ba895965add5da5
Miguel Enrique Bonilla Quishpe <64868918+MiguelBonilla814@users.noreply.github.com>
2022-02-16 23:45:26 -0500

    23:45

--- app/src/main/java/com/example/bonilla_quishpe_miguel_supletorio/ReportesActivity.java
+++ app/src/main/java/com/example/bonilla_quishpe_miguel_supletorio/ReportesActivity.java
@@ -1,10 +1,17 @@
 package com.example.bonilla_quishpe_miguel_supletorio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReportesActivity extends AppCompatActivity {
+
+    private List<Tarea> listaTareas = new ArrayList<Tarea>();
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/bonilla_quishpe_miguel_supletorio/Tarea.java
+++ app/src/main/java/com/example/bonilla_quishpe_miguel_supletorio/Tarea.java
@@ -1,6 +1,8 @@
 package com.example.bonilla_quishpe_miguel_supletorio;
 
 public class Tarea {
+
+    private int Id;
     private String Tarea;
     private String Detalle;
     private boolean Check;
@@ -9,9 +11,42 @@ public class Tarea {
 
     }
 
-    public Tarea(String tarea, String detalle, boolean check){
+    public Tarea(int id, String tarea, String detalle, boolean check){
+        Id = id;
         Tarea = tarea;
         Detalle = detalle;
+        Check = check;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getTarea() {
+        return Tarea;
+    }
+
+    public void setTarea(String tarea) {
+        Tarea = tarea;
+    }
+
+    public String getDetalle() {
+        return Detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        Detalle = detalle;
+    }
+
+    public boolean isCheck() {
+        return Check;
+    }
+
+    public void setCheck(boolean check) {
         Check = check;
     }
 }
