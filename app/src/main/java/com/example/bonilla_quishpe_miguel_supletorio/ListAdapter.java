73c90c4e39eb83386ac1d01b08a047e1b375b3e6
Miguel Enrique Bonilla Quishpe <64868918+MiguelBonilla814@users.noreply.github.com>
2022-02-16 22:30:27 -0500

    22:30

--- app/src/main/java/com/example/bonilla_quishpe_miguel_supletorio/ListAdapter.java
+++ app/src/main/java/com/example/bonilla_quishpe_miguel_supletorio/ListAdapter.java
@@ -1,12 +1,48 @@
 package com.example.bonilla_quishpe_miguel_supletorio;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class ListAdapter extends ArrayAdapter<Tarea> {
-    public List<Tarea> lista;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class ListAdapter {
+    /*public List<Tarea> lista;
     private Context contexto;
     private int resourceLayout;
 
+    public ListAdapter(@NonNull Context context, int resource, List<Modelo> objects){
+        super(context, resource, objects);
+        this.lista= objects;
+        this.contexto = context;
+        this.resourceLayout = resource;
+    }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        if(view == null){
+            view = LayoutInflater.form(contexto).inflate(R.layout.detalle_reportes, root null);
+
+            Tarea = lista.get(position);
+
+            TextView texto = view.findViewById(R.id.textView_titulo);
+            texto.setText("Hola");
+
+            TextView descripcion = view.findViewById(R.id.textView_Descripcion);
+            descripcion.setText("Nada que hacer perdido la materia :(");
+
+            CheckBox chekcbox = view.findViewById(R.id.checkBox_estado);
+            chekcbox.setChecked(true);
+
+        }
+    }*/
 }
